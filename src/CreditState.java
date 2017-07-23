357cc290d89dccfc3d54e836e8637a51960b8969
flavioshinzato <f.a.m.shinzato@gmail.com>
2017-07-23 14:04:04 -0400

    Adicionado as fontes customizadas

--- src/CreditState.java
+++ src/CreditState.java
@@ -1,8 +1,12 @@
+import java.awt.Font;
+
 import org.lwjgl.input.Mouse;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -10,6 +14,7 @@ public class CreditState extends BasicGameState{
 	private Image bg;
 	private int posX, posY;
 	private Button voltar;
+	private UnicodeFont ufont;
 
 	public CreditState() {
 	}
@@ -18,16 +23,55 @@ public class CreditState extends BasicGameState{
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		bg = new Image("background/bg2.png");
 		voltar = new Button("Voltar", 50, 130);
+		ufont = null;
 
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		bg.draw(0, 0, 800, 600);
-		g.drawString("Créditos:", 350, 200);
-		g.drawString("Flávio Augusto Müller Shinzato", 260, 220);
-		g.drawString("Jessica Abe", 330, 240);
-		g.drawString("Luiz Henrique Cavalcante da Silva", 260, 260);
+		
+		
+		String credit = "Creditos";
+		String name1 = "Flávio Augusto Müller Shinzato";
+		String name2 = "Jessica Abe";
+		String name3 = "Luiz Henrique Cavalcante da Silva";
+		try {
+		   Font font = new Font("OptimusPrincepsSemiBold", Font.BOLD, 30);
+		   ufont = new UnicodeFont(font, font.getSize(), font.isBold(), font.isItalic());
+		   ufont.addAsciiGlyphs();
+		   ufont.addGlyphs(400, 600);
+		   ufont.getEffects().add(new ColorEffect(java.awt.Color.white));
+		   ufont.loadGlyphs();
+		} catch (SlickException e) {
+		   e.printStackTrace();
+		}
+		g.setFont(ufont);
+		g.drawString(credit, 300, 160);
+		
+		try {
+			   Font font = new Font("OptimusPrincepsSemiBold", Font.BOLD, 15);
+			   ufont = new UnicodeFont(font, font.getSize(), font.isBold(), font.isItalic());
+			   ufont.addAsciiGlyphs();
+			   ufont.addGlyphs(400, 600);
+			   ufont.getEffects().add(new ColorEffect(java.awt.Color.white));
+			   ufont.loadGlyphs();
+			} catch (SlickException e) {
+			   e.printStackTrace();
+		}
+		
+		g.setFont(ufont);
+		g.drawString(name1, 260, 220);
+		g.drawString(name2, 330, 240);
+		g.drawString(name3, 260, 260);
+		
+		
+		//g.drawString("Créditos:", 350, 200);
+		//g.drawString("Flávio Augusto Müller Shinzato", 260, 220);
+		//g.drawString("Jessica Abe", 330, 240);
+		//g.drawString("Luiz Henrique Cavalcante da Silva", 260, 260);
+		
+		
 		voltar.getButton().draw(voltar.getX(),600-voltar.getY(), 189, 63);
 		posX = Mouse.getX();
 		posY = Mouse.getY();
--- src/MenuState.java
+++ src/MenuState.java
@@ -6,11 +6,16 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.Image;
 
+import java.awt.Font;
+import org.newdawn.slick.UnicodeFont;
+import org.newdawn.slick.font.effects.ColorEffect;
+
 public class MenuState extends BasicGameState{
 	private Image bg;
 	private Button playButton;
 	private Button creditButton;
 	private int posX, posY;
+	private UnicodeFont ufont;
 	
 	public MenuState() {
 	}
@@ -20,18 +25,32 @@ public class MenuState extends BasicGameState{
 		bg = new Image("background/bg2.png");
 		playButton = new Button("Jogar", 280, 380);
 		creditButton = new Button("Créditos", 280, 260);
-		
+		ufont = null;
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
 		bg.draw(0, 0, 800, 600);
-		g.drawString("Heroes of the Arena", 280, 200);
+		//g.drawString("Heroes of the Arena", 280, 200);
 		playButton.getButton().draw(playButton.getX(),600-playButton.getY(), 189, 63);
 		creditButton.getButton().draw(creditButton.getX(),600-creditButton.getY(), 189, 63);
 		
-
-		g.drawString(""+posX+" "+posY, 200, 200);
+		String title = "Heroes of the Arena";
+		try {
+		   Font font = new Font("OptimusPrincepsSemiBold", Font.BOLD, 30);
+		   ufont = new UnicodeFont(font, font.getSize(), font.isBold(), font.isItalic());
+		   ufont.addAsciiGlyphs();
+		   ufont.addGlyphs(400, 600);
+		   ufont.getEffects().add(new ColorEffect(java.awt.Color.white));
+		   ufont.loadGlyphs();
+		} catch (SlickException e) {
+		   e.printStackTrace();
+		}
+		g.setFont(ufont);
+		g.drawString(title, 230, 150);
+		//ufont.drawString(450, 450, cad);
+		
+		//g.drawString(""+posX+" "+posY, 200, 200);
 		
 	}
 
